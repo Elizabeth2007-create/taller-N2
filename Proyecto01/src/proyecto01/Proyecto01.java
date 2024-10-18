181b2b20af0957a707f92520dd2aaff464277514
Elizabeth2007-create <chunchograndavaleriaelizabeth@gmail.com>
2024-10-17 22:34:23 -0500

    Mi segunda revisón

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto01;
-
-/**
- *
- * @author D E L L
- */
 public class Proyecto01 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Manejado desde Git gub versiopnado a traves de git Creado por Valeria Chuncho"); 
+                
     }
     
 }
